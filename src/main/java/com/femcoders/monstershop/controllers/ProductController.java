55563a2846898df1398b65e5709afb3c216392b1
Bruna Sonda <bsonda@gmail.com>
2025-06-16 15:22:37 +0200

    fifth commit: add controllers

--- src/main/java/com/femcoders/monstershop/controllers/ProductController.java
+++ src/main/java/com/femcoders/monstershop/controllers/ProductController.java
@@ -1,4 +1,22 @@
 package com.femcoders.monstershop.controllers;
+import com.femcoders.events.models.Event;
+import com.femcoders.events.services.EventService;
+import com.femcoders.monstershop.services.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
 public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
 }
