37b50d9bc5a4e3fb9a0a23ea59d2d9ad95b1ab36
Bruna Sonda <bsonda@gmail.com>
2025-06-16 15:17:49 +0200

    fourth commit: add services

--- src/main/java/com/femcoders/monstershop/services/ProductService.java
+++ src/main/java/com/femcoders/monstershop/services/ProductService.java
@@ -1,4 +1,15 @@
 package com.femcoders.monstershop.services;
+import com.femcoders.monstershop.models.Product;
+import com.femcoders.monstershop.repositories.ProductRepository;
+import jdk.jfr.Event;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 public class ProductService {
+    private final ProductRepository productRepository;
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+
+    }
 }
