c506f4914f2bc70817e7888db3b6cfcd06cc5334
Bruna Sonda <bsonda@gmail.com>
2025-06-17 12:31:00 +0200

    add feature add reviews

--- src/main/java/com/femcoders/monstershop/controllers/ReviewController.java
+++ src/main/java/com/femcoders/monstershop/controllers/ReviewController.java
@@ -1,7 +1,14 @@
 package com.femcoders.monstershop.controllers;
 
 import com.femcoders.monstershop.services.ReviewService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ReviewController {
@@ -11,4 +18,11 @@ public class ReviewController {
     public ReviewController(ReviewService reviewService) {
         this.reviewService = reviewService;
     }
+
+    @PostMapping("/api/reviews")
+    public ResponseEntity<ReviewResponse> addCategory (@Valid @RequestBody ReviewRequest categoryRequest) {
+        ReviewResponse newReview = reviewService.addReview(reviewRequest);
+        return new ResponseEntity<>(newReview, HttpStatus.CREATED);
+    }
+
 }
--- src/main/java/com/femcoders/monstershop/services/ReviewService.java
+++ src/main/java/com/femcoders/monstershop/services/ReviewService.java
@@ -1,5 +1,6 @@
 package com.femcoders.monstershop.services;
 
+import com.femcoders.monstershop.dtos.review.ReviewResponse;
 import com.femcoders.monstershop.repositories.ReviewRepository;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +9,12 @@ public class ReviewService {
     private  final ReviewRepository reviewRepository;
     public ReviewService(ReviewRepository reviewRepository) {
         this.reviewRepository = reviewRepository;
+    }
+
+    public ReviewResponse addReview(ReviewRequest categoryRequest){
+        Review newReview = ReviewMapper.dtoToEntity(categoryRequest);
+        Review savedReview = ReviewRepository.save(newReview);
+        return ReviewMapper.entityToDto(ReviewCategory);
     }
 
 }
